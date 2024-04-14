bfd9558646ad1559eb20d9c2bdb14970ad9b9226
amalg016 <86319979+Amalg016@users.noreply.github.com>
2024-04-15 01:13:01 +0530

    init

---
+++ nvim/ftplugin/java.lua
@@ -0,0 +1,84 @@
+-- java setup
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local home = os.getenv 'HOME'
+local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local config = {
+    -- The command that starts the language server
+    -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+    cmd = {
+        'java',
+        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '-Dosgi.bundles.defaultStartLevel=4',
+        -- '-Dosgi.requiredJavaVersion=11',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '-Dlog.level=ALL',
+        '-noverify',
+        '-Xmx1G',
+        '--add-modules=ALL-SYSTEM',
+        --'--add-modules=java.base,java.logging,java.sql,java.xml,java.desktop,java.management,java.naming,java.rmi,java.scripting,java.security.jgss,java.security.sasl,java.sql.rowset,java.transaction.xa,java.xml.bind,java.xml.crypto,jdk.httpserver,jdk.unsupported,jdk.zipfs',
+        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+        '-jar', home ..
+    '/.cache/nvim/nvim_lsp/jdtls/plugins/org.eclipse.equinox.launcher_1.5.700.v20200207-2156.jar',
+        '-configuration', home .. '/.cache/nvim/nvim_lsp/jdtls/config_mac',
+        '-data', vim.fn.expand('~/.cache/jdtls-workspace') .. workspace_dir,
+        -- See `data directory configuration` section in the README
+    },
+    -- This is the default if not provided, you can remove it. Or adjust as needed.
+    -- One dedicated LSP server & client will be started per unique root_dir
+    root_dir = vim.fs.dirname(vim.fs.find({ '.git', 'mvnw', 'gradlew' }, { upward = true })[1]),
+    capabilities = lsp_capabilities,
+    init_options = {
+        bundles = {
+            vim.fn.glob(
+                '/Users/amal-18877/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-0.50.0.jar')
+        }
+    }
+
+}
+
+config['on_attach'] = function(client, bufnr)
+    require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+end
+
+-- https://download.eclipse.org/jdtls/milestones/       -0.57 version
+require('jdtls').start_or_attach(config)
+
+function attach_to_debug()
+    local dap = require('dap')
+    -- dap.adapters.java = {
+    --     type = 'executable',
+    --     command = 'java',
+    --     args = { '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=localhost:8000' },
+    --     name = "Java Debug",
+    -- }
+    dap.configurations.java = {
+        {
+            type = 'java',
+            request = 'attach',
+            name = "Attach to Java Process",
+            hostName = "localhost",
+            port = 8000
+        }
+    }
+    dap.continue()
+    print("attached")
+end
+
+function run_app(debug)
+    if debug then
+        vim.cmd('term' .. './gradlew startTomcat -Pdebug=true')
+    else
+        vim.cmd('term' .. './gradlew startTomcat')
+    end
+    -- vim.cmd(':bprevious<CR>')
+end
+
+-- Attach debug Session
+vim.keymap.set("n", "<Leader>ll", ':lua attach_to_debug()<CR>')
+
+-- Debug
+vim.keymap.set("n", "<F10>", ':lua run_app(true)<CR>')
+
+-- Run
+vim.keymap.set("n", "<F9>", ':lua run_app()<CR>')
---
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,135 @@
+-- LSP Support
+return {
+    -- LSP Configuration
+    -- https://github.com/neovim/nvim-lspconfig
+    'neovim/nvim-lspconfig',
+    event = 'VeryLazy',
+    dependencies = {
+        -- LSP Management
+        -- https://github.com/williamboman/mason.nvim
+        { 'williamboman/mason.nvim' },
+        -- https://github.com/williamboman/mason-lspconfig.nvim
+        { 'williamboman/mason-lspconfig.nvim' },
+        -- Useful status updates for LSP
+        -- https://github.com/j-hui/fidget.nvim
+        { 'j-hui/fidget.nvim',                opts = {} },
+        { 'mfussenegger/nvim-jdtls' },
+        -- Additional lua configuration, makes nvim stuff amazing!
+        -- https://github.com/folke/neodev.nvim
+        { 'folke/neodev.nvim' },
+    },
+    config = function()
+        require('mason').setup()
+        require('mason-lspconfig').setup({
+            -- Install these LSPs automatically
+            ensure_installed = {
+                'bashls', -- requires npm to be installed
+                -- 'cssls', -- requires npm to be installed
+                -- 'html', -- requires npm to be installed
+                'gradle_ls',
+                'lua_ls',
+                -- 'intelephense', -- requires npm to be installed
+                -- 'jsonls', -- requires npm to be installed
+                --'lemminx',
+                'marksman',
+                'tsserver',
+                -- 'prettier',
+                'eslint'
+                --  'rust_analyzer'
+                -- 'tsserver', -- requires npm to be installed
+                -- 'yamlls', -- requires npm to be installed
+            }
+        })
+
+        local lspconfig = require('lspconfig')
+        local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local lsp_attach = function(client, bufnr)
+            -- Create your keybindings here...
+        end
+
+        -- Call setup on eacah LSP server
+        require('mason-lspconfig').setup_handlers({
+            function(server_name)
+                lspconfig[server_name].setup({
+                    on_attach = lsp_attach,
+                    capabilities = lsp_capabilities,
+                    handlers = {
+                        -- Add borders to LSP popups
+                        ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' }),
+                        ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help,
+                            { border = 'rounded' }),
+                    }
+                })
+            end
+        })
+
+
+
+        -- js, ts
+        lspconfig.tsserver.setup {}
+
+        -- marksman
+        lspconfig.marksman.setup {}
+
+        -- java setup
+        --local jdtls_ok, jdtls = pcall(require, "jdtls")
+        --if not jdtls_ok then
+        --    vim.notify "JDTLS not found, install with `:LspInstall jdtls`"
+        --    return
+        --end
+        -- local home = os.getenv 'HOME'
+        -- local jdtls_path = vim.fn.stdpath('data') .. "/lsp_servers/jdtls"
+        -- local path_to_lsp_server = jdtls_path .. "/config_mac"
+        --local config = {
+        --    -- The command that starts the language server
+        --    -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+        --    cmd = {
+        --        '-java',
+        --        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        --        '-Dosgi.bundles.defaultStartLevel=4',
+        --        '-Dosgi.requiredJavaVersion=11',
+        --        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        --        '-Dlog.protocol=true',
+        --        '-Dlog.level=ALL',
+        --        '-Xmx1g',
+        --        '--add-modules=ALL-SYSTEM',
+        --        --'--add-modules=java.base,java.logging,java.sql,java.xml,java.desktop,java.management,java.naming,java.rmi,java.scripting,java.security.jgss,java.security.sasl,java.sql.rowset,java.transaction.xa,java.xml.bind,java.xml.crypto,jdk.httpserver,jdk.unsupported,jdk.zipfs',
+        --        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+        --        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+        --        '-jar',
+        --        '/Users/amal-18877/state/nvim/lsp/jdt-language-server-0.57.0-202006172108/plugins/org.eclipse.equinox.launcher_*.jar',
+        --        '-configuration', '/Users/amal-18877/state/nvim/lsp/jdt-language-server-0.57.0-202006172108/config_mac/'
+        --        -- See `data directory configuration` section in the README
+        --    },
+
+        --    -- This is the default if not provided, you can remove it. Or adjust as needed.
+        --    -- One dedicated LSP server & client will be started per unique root_dir
+
+        --    root_dir = lspconfig.util.root_pattern("build.gradle", ".git", "settings.gradle", "gradlew"),
+        --}
+
+        --nvimjdtls.setup(config)
+
+        --java
+        -- lspconfig.gradle_ls.setup {
+        --     cmd = { "gradle-language-server" },
+        --     filetypes = { "gradle", "java" },
+        --     root_dir = lspconfig.util.root_pattern("build.gradle", ".git", "settings.gradle", "gradlew"),
+        -- }
+        -- Lua LSP settings
+        lspconfig.lua_ls.setup {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = { 'vim' },
+                    },
+                },
+            },
+        }
+    end
+}
+
+-- root_dir = vim.fs.dirname(vim.fs.find({ '.git', 'mvnw', 'gradlew' }, { upward = true })[1]),
+--'/Users/amal-18877/.local/share/nvim/mason/packages/openjdk-17/jdk-17.0.2.jdk/Contents/Home/bin/'
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
